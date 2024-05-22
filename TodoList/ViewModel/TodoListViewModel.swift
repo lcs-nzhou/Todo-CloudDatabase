2c3467e57726f8c876f3a8e9862cb3fa8347b5e1
Nina Zhou <nzhou@lcs.on.ca>
2024-05-22 19:50:47 -0400

    Adjusted the view model so that newly added to-do items are persisted.

--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -10,55 +10,41 @@ import Foundation
 @Observable
 class TodoListViewModel {
     
-    // MARK: Stored properties
-    // The list of to-do items
-    var todos: [TodoItem]
-    
-    // MARK: Initializer(s)
-    init(todos: [TodoItem] = []) {
-        self.todos = todos
+    func createToDo(withTitle title: String) {
+        
+        // Create a unit of asynchronous work to add the to-do item
         Task {
-                 try await getTodos()
-             }
-    }
-    
-    // MARK: Functions
-    func getTodos() async throws {
             
+            // Create the new to-do item instance
+            // NOTE: The id will be nil for now
+            let todo = TodoItem(
+                title: title,
+                done: false
+            )
+            
+            // Write it to the database
             do {
-                let results: [TodoItem] = try await supabase
-                    .from("todos")
-                    .select()
-                    .execute()
-                    .value
                 
-                self.todos = results
+                // Insert the new to-do item, and then immediately select
+                // it back out of the database
+                let newlyInsertedItem: TodoItem = try await supabase
+                    .from("todos")
+                    .insert(todo)   // Insert the todo item created locally in memory
+                    .select()       // Select the item just inserted
+                    .single()       // Ensure just one row is returned
+                    .execute()      // Run the query
+                    .value          // Automatically decode the JSON into an instance of TodoItem
+
+                // Finally, insert the to-do item instance we just selected back from the
+                // database into the array used by the view model
+                // NOTE: We do this to obtain the id that is automatically assigned by Supabase
+                //       when the to-do item was inserted into the database table
+                self.todos.append(newlyInsertedItem)
                 
             } catch {
                 debugPrint(error)
             }
-            
         }
-    func createToDo(withTitle title: String) {
-        
-        // Create the new to-do item instance
-        let todo = TodoItem(
-            title: title,
-            done: false
-        )
-        
-        // Append to the array
-        todos.append(todo)
-        
-    }
-    
-    func delete(_ todo: TodoItem) {
-        
-        // Remove the provided to-do item from the array
-        todos.removeAll { currentItem in
-            currentItem.id == todo.id
-        }
-        
     }
     
 }
